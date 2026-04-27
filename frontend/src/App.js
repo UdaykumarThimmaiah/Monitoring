073c79676b1e67159b5ecaae9a6b41d4971d61ed
UdaykumarThimmaiah <udaykumar.t@starmarkit.com>
2026-04-27 19:20:58 +0530

    Create App.js file for the monitoring dashboard application on 2026-04-27 13:50:16 UTC.

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Monitoring!</h1>
+      <p>This is your starter SolarWinds-like dashboard application.</p>
+    </div>
+  );
+}
+
+export default App;
